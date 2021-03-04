ebd912403040ef2e169b98db8e66d12cb11803b3
nimowairimu <nimowairimu25@gmail.com>
2021-03-05 01:48:19 +0300

    remove error prone dependencies.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User,Pitch,pitches
+from ..models import User,Pitch
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-from ..models import User,Pitch,pitches,Comment,Upvote,Downvote
+from ..models import User,Pitch,Comment,Upvote,Downvote
 from .forms import PitchForm,CommentForm
 from .. import db,photos
 from flask_login import login_required, current_user
