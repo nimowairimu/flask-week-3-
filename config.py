62b8d389c012417c14b3f8b63707a3de4cb92b83
nimowairimu <nimowairimu25@gmail.com>
2021-03-04 16:18:54 +0300

    Second Migartion.

--- config.py
+++ config.py
@@ -27,7 +27,8 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MAIL_USERNAME='nimowairimu25@gmail.com'
+
 export MAIL_PASSWORD='kadesho62'
 export SECRET_KEY='moringa123'
 python3.6 manage.py server
