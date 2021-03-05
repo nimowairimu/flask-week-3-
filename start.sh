793f3656dfa0549c3384d9b7adb6b6f4b9278fa2
nimowairimu <nimowairimu25@gmail.com>
2021-03-05 13:35:20 +0300

    Delete pitch posted.

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ export MAIL_USERNAME='nimowairimu25@gmail.com'
 export MAIL_PASSWORD='kadesho62'
 export SECRET_KEY='moringa123'
 # export DATABASE_URL='postgres://oldciaqzbfelvi:b167cd8bbc01bcbfb83ed35ab94c986c29e946a3f2cb76214c8f84a04ef246d5@ec2-54-162-119-125.compute-1.amazonaws.com:5432/dcqqnck6utumhn'
-export DATABASE_URL='postgresql+psycopg2://nimo:kadesho62@localhost/pitch34'
+export DATABASE_URL='postgresql+psycopg2://nimo:kadesho62@localhost/pitch35'
 python3.6 manage.py server
