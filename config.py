57fd7847f1109a885bf5ccfc255f7d8a8e326ea8
nimowairimu <nimowairimu25@gmail.com>
2021-03-05 07:29:03 +0300

    Update changes on the apps configurations.

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
 class DevConfig(Config):
     '''
@@ -39,7 +39,7 @@ class DevConfig(Config):
     '''
 
    
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
     DEBUG = True
 
